3dc661fbb9f59224b64d9bae71825e50ba8d15ce
Larin2023 <alexander.v.kolesnyk@gmail.com>
2023-11-03 16:12:06 -0400

    second commit

---
+++ Part4b_index_JavaScript_Variables_Data Types.js
@@ -0,0 +1,82 @@
+//        //"var" keyword
+
+//        // multiple line
+//        var x;  // declaration
+//        x=10;   // initialization
+//
+//        //single statement - declaration+initialization
+//        var x=10;
+//
+//        //single statement - multiple variable
+//        var x=100,y=200;
+//        document.write(x);
+//        console.log(x);
+
+
+
+
+//        // "let" keyword
+
+//        // multiple line
+//         let x;
+//         x="Welcome";
+//        console.log(x);
+
+//        //single statement
+//        let x="welcome";
+//        console.log(x);
+
+
+
+//        // Multiple variable in single line
+//        var x="Welcome",y=100;
+//        console.log(x);
+//        console.log(y);
+
+
+
+
+//        // Difference between var and let
+//
+//        // var is function scope (undifine) and let is block scope  (error)
+//
+//        console.log(x);
+//
+//        // var x=100;
+//        let x=100;   // error
+//        console.log(x);
+
+
+
+
+//        //        //"const" keyword
+//        const x=100;
+//        console.log(x);
+//        x=500;       // incorrect because const value cannot be changed
+//        console.log(x);
+
+
+
+
+
+//        // ***Data types
+//        // primitive data types-----Number, String, Boolean, Null, Undefined
+//        // Non-Premitive(Objects)------ Object, Array, Function, Date, Regx
+//
+//         let x=100;
+//         console.log(typeof(x));
+//
+//         let price=12.9;
+//         console.log(typeof(price));
+//
+//         let studentName="John";
+//         console.log(typeof(studentName));
+//
+//         let flag=true;
+//         console.log(typeof(flag));//boolean
+//
+//         let status=null;
+//         console.log(typeof(status));  //object
+//
+//        let value;
+//        console.log(typeof(value));  //undefined
